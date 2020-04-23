a9967f96294a6bbf2ed68f9d37c93323a77b2717
NoraLou <nora.humpage@gmail.com>
2020-04-23 15:12:06 -0400

    set up main routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import {Route, Switch } from 'react-router-dom';
 import './App.scss';
 import HomePage from './pages/homepage.comp'
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' render={routeProps => <HomePage {...routeProps}/>}/>
+        <Route exact path='/hats' render={routeProps => <HatsPage {...routeProps}/>}/>
+      </Switch>
     </div>
   );
 }
+
 
 export default App;
--- src/pages/homepage.comp.jsx
+++ src/pages/homepage.comp.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Directory from '../components/directory/directory.comp'
 import './homepage.styles.scss';
 
-const HomePage = () => {
+
+const HomePage = ({history}) => {
   return (
     <div className="homepage">
       <Directory />
