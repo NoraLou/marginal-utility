04d6477ea69b74f028e180eaa221c2c9eaee231a
NoraLou <nora.humpage@gmail.com>
2020-04-27 15:26:07 -0400

    added in header comp

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import {Route, Switch } from 'react-router-dom';
 import './App.scss';
 import HomePage from './pages/homepage/homepage.comp'
 import ShopPage from './pages/shoppage/shoppage.comp'
+import Header from './components/header/header.comp'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path='/' render={routeProps => <HomePage {...routeProps}/>}/>
         <Route exact path='/shoppage' render={routeProps => <ShopPage {...routeProps}/>}/>
