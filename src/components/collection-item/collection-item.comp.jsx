a8ab39d6510b8f2663b545d5f6cf2e493e434fe3
NoraLou <nora.humpage@gmail.com>
2020-05-08 15:46:57 -0400

    clean up test data,  added utils and ability to group items in the cart reducer

--- src/components/collection-item/collection-item.comp.jsx
+++ src/components/collection-item/collection-item.comp.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import './collection-item.styles.scss';
+import {connect} from 'react-redux';
+import { addCartItem } from '../../redux/cart/cart.actions';
+import CustomButton from '../custom-button/custom-button.comp';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-  <div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+const CollectionItem = ({item, addCartItem}) => {
+  const {imageUrl, name, price} = item;
+  return (
+    <div className='collection-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <CustomButton
+        inverted
+        onClick= {()=>addCartItem(item)}
+      >
+        Add to Cart
+      </CustomButton>
     </div>
-  </div>
-);
+)};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addCartItem: cartItem => dispatch(addCartItem(cartItem))
+});
+
+
+export default connect (null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.actions.jsx
+++ src/redux/cart/cart.actions.jsx
@@ -2,4 +2,9 @@ import CartActionTypes  from './cart.types';
 
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
-})
+});
+
+export const addCartItem = (cartItem) => ({
+  type: CartActionTypes.ADD_CART_ITEM,
+  payload: cartItem
+});
