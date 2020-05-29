88ab04ee3b83d88b88ceff8c3c2049edcb82dad6
NoraLou <nora.humpage@gmail.com>
2020-05-29 16:39:09 -0400

    added in functions to decrement/increment cartItems

--- src/components/checkout-item/checkout-item.comp.jsx
+++ src/components/checkout-item/checkout-item.comp.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './checkout-item.styles.scss';
 import { connect } from 'react-redux';
-import { clearItemFromCart, removeItem } from '../../redux/cart/cart.actions';
+import { clearItemFromCart, removeItem, addCartItem } from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({cartItem, clearItem}) => {
+const CheckoutItem = ({cartItem, clearItemFromCart, removeItem, addCartItem}) => {
   const { name, imageUrl, price, quantity} = cartItem;
   return (
       <div className='checkout-item'>
@@ -12,13 +12,19 @@ const CheckoutItem = ({cartItem, clearItem}) => {
         </div>
         <span className='name'>{name}</span>
         <span className='quantity'>
-          <div className='arrow'>&#10094;</div>
+          <div className='arrow'
+            onClick={()=>removeItem(cartItem)}>
+            &#10094;
+          </div>
             <span className='value'>{quantity}</span>
-          <div className='arrow'>&#10095;</div>
+          <div className='arrow'
+            onClick={()=>addCartItem(cartItem)}>
+            &#10095;
+          </div>
         </span>
         <span className='price'>{price}</span>
         <span className='remove-button'
-          onClick={()=>clearItem(cartItem)}>&#10005;</span>
+          onClick={()=>clearItemFromCart(cartItem)}>&#10005;</span>
       </div>
     )
 };
